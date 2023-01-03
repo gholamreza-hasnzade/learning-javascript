fa6985198308a4ca8a3e9c950abf5d8dafe810c4
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-03 10:22:21 +0330

    Hoisting

--- script/script.js
+++ script/script.js
@@ -1,4 +1,18 @@
+// * Hoisting - Hoist - بالا بردن
+/* var userAge;
 userAge = 28;
-console.log(typeof userAge);
+console.log( userAge); */
+/////////////////////////////////////////////////////////
 
-var userAge;
+/* showValue()
+function showValue() {
+    console.log("this is test value :)");
+} */
+// * Function Declaration (به معنی اعلام تابع) نیز از قانون  following hosting
+/////////////////////////////////////////////////////////
+
+/* showValue()
+var showValue = function () {
+    console.log("this is test value :)");
+}; */
+// *  Function Expression  not following hosting
