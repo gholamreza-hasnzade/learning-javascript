84c5c6bbda23994c547164f6a44ad54c64ece6e5
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-03 12:08:39 +0330

    arrow functions

--- script/script.js
+++ script/script.js
@@ -34,10 +34,8 @@ var num1; */ // * following hosting
 /* num2 = 20;
 console.log("num2:", num2);
 let num1; */ // * not following hosting
-
 /* var num1 = 10;
 let num2 = 20;
-
 var num1 = 30;
 let num2 = 40;
 console.log("num1:", num1); // * success but not true
@@ -45,17 +43,62 @@ console.log("num2:", num2); // *  has already been declared error
 */
 // * scope global
 /* let num1 = 10;
-
 {
     // * scope local
     let num1 = 12;
     console.log(num1);
 }
 console.log(num1); */
-var num1 = 10;
+/* var num1 = 10;
 let num2 = 20;
 const num3 = 30;  // * not update or not chnage value and like the let varibale
-
 console.log("num1:", num1);
 console.log("num2:", num2);
-console.log("num3:", num3); 
+console.log("num3:", num3);  */
+/////////////////////////////////////////////////////////
+// * arrow function
+
+/*
+ * not following hosting
+ * 1 -just one line run
+ **/
+
+/* let sum = (num1, num2) => {
+    num1 + num2;
+};
+console.log("sun:", sum(2, 12)); */
+
+/* let doubleNumber = (num) => num * 2;
+
+console.log("doubleNumber", doubleNumber(2)); */
+
+/* let makeUser = (id, name) => ({
+    id: id,
+    name: name,
+})
+
+console.log("doubleNumber", makeUser(1, "Raza"));  */
+
+// constructor  function
+/* function User () {
+}
+
+let user1 = new User()
+console.log(user1);
+ */
+
+
+/* let users = [
+    { id: 1, name: "ali", age: 25 },
+    { id: 2, name: "madd", age: 26 },
+    { id: 3, name: "hassan", age: 27 },
+];
+
+users.forEach(function (user) {
+    console.log(user);
+});
+
+let mainUser = users.find(user => user.name === "ali")
+console.log(mainUser); */
+
+/////////////////////////////////////////////////////////
