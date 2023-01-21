a03c84868344cf539dd22092d8f4fca342639fb3
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-21 09:25:41 +0330

    get data fetch

--- script/script.js
+++ script/script.js
@@ -635,3 +635,10 @@ async function init() {
 }
 init(); */
 //////////////////////////////////////////////
+/*
+ * fetch
+ */
+/* fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+ */
